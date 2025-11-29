edfff002359e550e8dbdded34868c8973cc00fcb
donus1perdonus <ekzozeta@gmail.com>
2025-11-30 01:51:38 +0300

    refactor

--- mp_os/common/src/not_implemented.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/not_implemented.h"
-
-not_implemented::not_implemented(
-    std::string const &method_name,
-    std::string const &message):
-    logic_error("method `" + method_name + "` not implemented: \"" + message + "\"")
-{
-
-}
